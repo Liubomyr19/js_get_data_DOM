aae6ebfc62a7a5d7546765a903bb00d9a3059804
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-01-16 13:50:46 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const nodeList = document.querySelectorAll(".population");
+const elemenTotal = document.querySelector(".total-population");
+const elemAverage = document.querySelector(".average-population");
+const population = [
+    ...nodeList
+].map((elem)=>+elem.innerText.replaceAll(",", ""));
+const total = population.reduce((a, b)=>a + b, 0);
+const avarage = (total / population.length).toLocaleString();
+elemenTotal.innerText = total.toLocaleString();
+elemAverage.innerText = avarage;
+
+//# sourceMappingURL=index.f75de5e1.js.map
